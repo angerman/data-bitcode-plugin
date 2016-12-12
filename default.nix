40481d43e2e5b2f788732b3d0d0c76b469dd25c9
Moritz Angermann <moritz.angermann@gmail.com>
2016-12-12 20:56:32 +0800

    restructure

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
  ghc = (nixpkgs.callPackage ./ghc {}).pluginGhc;
- data-bitcode = ghc.callPackage ../data-bitcode/data-bitcode.nix { };
- data-bitcode-llvm = ghc.callPackage ../data-bitcode-llvm/data-bitcode-llvm.nix { inherit data-bitcode; };
- data-bitcode-edsl = ghc.callPackage ../data-bitcode-edsl/data-bitcode-edsl.nix { inherit data-bitcode data-bitcode-llvm; };
+ data-bitcode = ghc.callPackage ./data-bitcode { };
+ data-bitcode-llvm = ghc.callPackage ./data-bitcode-llvm { inherit data-bitcode; };
+ data-bitcode-edsl = ghc.callPackage ./data-bitcode-edsl { inherit data-bitcode data-bitcode-llvm; };
 in
  ghc.callPackage  ./data-bitcode-plugin.nix { inherit data-bitcode-edsl data-bitcode-llvm; }
